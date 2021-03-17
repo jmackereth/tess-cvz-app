637d82507e0992f63bc08583acd6551ee9d822b5
jmackereth <tedmackereth@gmail.com>
2021-03-17 17:37:09 -0400

    script to grab the data

---
+++ data/get_data.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#grab the data for the webapp from zenodo.
+
+wget https://zenodo.org/record/4612265/files/TESS_CVZ_brightgiants_goodsample.dat
+
+wget https://zenodo.org/record/4612265/files/standardised_psds_goodsample.npy
